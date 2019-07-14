d40b1033e5dcd3ab3d4147177cd160dbb542a157
Kamil Frydlewicz <kamil@frydlewicz.pl>
2019-07-14 12:34:13 +0200

    deleted from git compiled js files

--- model/Main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Status;
-(function (Status) {
-    Status["success"] = "SUCCESS";
-    Status["failure"] = "FAILURE";
-    Status["duplicate"] = "DUPLICATE";
-})(Status = exports.Status || (exports.Status = {}));
